d372d2f93d8504e077dcacb55feb54a6a3d62c88
Ayoola Aina <ayo@webraiders.co>
2025-10-03 00:56:54 +0100

    implemented header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      Home
+      </div>
+  )
+}
+
+export default Home
+
+// export default function Home() {
+//   return (
+//     <div className="flex min-h-screen home-wrapper">
+//       <h1 className="text-4xl font-bold">Welcome to Stock Signals</h1>
+//     </div>
+//   );
+// }
